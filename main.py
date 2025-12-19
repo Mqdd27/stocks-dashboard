3f9d9bec2ec316172c95559642ddeb3e2ee30faa
Mqdd <ahmadmiqdad27@gmail.com>
2025-12-19 17:04:09 +0700

    <Miqdad> add chart

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ def index():
     return render_template("index.html")
 
 @app.route("/api/stock")
-def stock():
-    symbol = request.args.get("symbol", "").upper()
+def api_stock():
+    symbol = request.args.get("symbol")
     period = request.args.get("period", "1mo")
+    interval = request.args.get("interval", "15m")
 
-    if not symbol:
-        return jsonify({"error": "symbol required"}), 400
+    data = get_stock(symbol, period, interval)
 
-    data = get_stock(symbol, period)
     if not data:
-        return jsonify({"error": "data not found"}), 404
+        return jsonify({"error": "Data tidak ditemukan"})
 
     return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- services/yahoo.py
+++ services/yahoo.py
@@ -1,27 +1,43 @@
 import yfinance as yf
 from functools import lru_cache
+from datetime import time
+
+IDX_OPEN = time(9, 0)
+IDX_CLOSE = time(15, 50)
 
 def get_stock(symbol, period="1mo", interval="15m"):
     ticker = yf.Ticker(symbol)
-    hist = ticker.history(period=period, interval=interval)
+
+    hist = ticker.history(
+        period=period,
+        interval=interval,
+        auto_adjust=False
+    )
 
     if hist.empty:
         return None
 
+    # filter jam bursa IDX (hanya intraday)
+    if interval.endswith("m") or interval.endswith("h"):
+        hist = hist.between_time(IDX_OPEN, IDX_CLOSE)
+
+    last = hist.iloc[-1]
+
     return {
         "symbol": symbol,
-        "last_price": float(hist["Close"].iloc[-1]),
-        "open": float(hist["Open"].iloc[-1]),
-        "high": float(hist["High"].iloc[-1]),
-        "low": float(hist["Low"].iloc[-1]),
-        "volume": int(hist["Volume"].iloc[-1]),
+        "last_price": round(float(last["Close"]), 2),
+        "open": round(float(last["Open"]), 2),
+        "high": round(float(last["High"]), 2),
+        "low": round(float(last["Low"]), 2),
+        "volume": int(last["Volume"]),
+
         "history": [
             {
-                "date": idx.strftime("%Y-%m-%d-%H:%M"),
-                "open": float(row["Open"]),
-                "high": float(row["High"]),
-                "low": float(row["Low"]),
-                "close": float(row["Close"]),
+                "time": idx.strftime("%Y-%m-%d %H:%M"),
+                "open": round(float(row["Open"]), 2),
+                "high": round(float(row["High"]), 2),
+                "low": round(float(row["Low"]), 2),
+                "close": round(float(row["Close"]), 2),
                 "volume": int(row["Volume"]),
             }
             for idx, row in hist.iterrows()
